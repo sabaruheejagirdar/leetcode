c1e9d022e16dfbae15ce7cb5b838f996bae51502
sabaruheejagirdar <sabaruheejagirdar@gmail.com>
2024-04-17 06:15:38 -0500

    Arrays and Hashing : 1 Contains Duplicate

---
+++ src/_1Arrays_Hashing/_1ContainsDuplicate.java
@@ -0,0 +1,41 @@
+package _1Arrays_Hashing;
+
+/*STRATEGY:
+
+Example1
+Input: nums = [1,2,3,1]
+Output: true
+
+Example2
+Input: nums = [1,2,3,4]
+Output: false
+
+* 1. Create a HashSet to keep track of visited elements
+* 2. If element exists in the HashSet, return true
+        Else, add the element to the HashSet
+* 3. All elements traversed and no duplicate found in HashSet, return false
+* */
+
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class _1ContainsDuplicate {
+
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4};
+        System.out.println(containsDuplicate(nums));
+    }
+
+    public static boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> numSet = new HashSet<Integer>();
+
+        for(int i = 0; i<nums.length; i++){
+            if(numSet.contains(nums[i])){
+                return true;
+            }
+            numSet.add(nums[i]);
+        }
+        return false;
+    }
+}
